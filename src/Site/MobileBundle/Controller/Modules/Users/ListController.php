3db409678ade4d006ed4583e86681b20ff1fe7bc
Core <hfreedomx@gmail.com>
2014-05-12 02:34:29 +0800

    [Add a new bundle]

--- src/Site/MobileBundle/Controller/Modules/Users/ListController.php
+++ src/Site/MobileBundle/Controller/Modules/Users/ListController.php
@@ -5,9 +5,21 @@ namespace Site\MobileBundle\Controller\Modules\Users;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
 class ListController extends Controller
 {
+    /**
+     * @Route("/all/{name}")
+     */
     public function allAction ($name) {
         return new Response('<html><body>Hello '.$name.'!</body></html>');
+    }
+
+    /**
+     * @Route("/regular")
+     */
+    public function regularAction () {
+        return new Response('<html><body>Regular List</body></html>');
     }
 }
